31b51024325ccbaa4253486bc590624390e7e694
ayoub-mabrouk <ayoubmabroukcontact@gmail.com>
2021-06-12 13:46:47 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Ayoub Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"  @end="endGame"/>
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+export default {
+  name: "App",
+  components: {Block, Results},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score:null,
+      showResults:false
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+      this.showResults=false
+    },
+    endGame(reactionTime){
+this.score=reactionTime
+this.isPlaying=false
+this.showResults=true
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>Reaction Time - {{ score }} ms</p>
+  <p class="rank">- {{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    this.rank =
+      this.score < 175
+        ? "You Are Extremely FAST" +
+          String.fromCodePoint(128580) +
+          String.fromCodePoint(128552)
+        : this.score < 225
+        ? "You are Faster than the average" + String.fromCodePoint(128515)
+        : this.score < 275
+        ? "You Are Around average " + String.fromCodePoint(128515)
+        : "You Got Snail's Speed" + String.fromCodePoint(128514)+String.fromCodePoint(128012);
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: 0faf87;
+  font-weight: bold;
+}
+</style>
